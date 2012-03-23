5f67b23405a1d11584c8354d95ca7d9dc14b6474
Benoit Moussaud <bmoussaud@xebialabs.com>
2012-03-23 11:45:00 +0100

    add tests for HierarchicalDictionary

--- src/main/java/com/xebialabs/deployit/plugins/dictionary/HierarchicalDictionary.java
+++ src/main/java/com/xebialabs/deployit/plugins/dictionary/HierarchicalDictionary.java
@@ -16,7 +16,7 @@ import static com.xebialabs.deployit.plugin.api.udm.Metadata.ConfigurationItemRo
 @Metadata(root = ENVIRONMENTS, description = "A Dictionary contains list of dictionaries that will be interrogated following the order of a list of other dictionaries.")
 public class HierarchicalDictionary extends Dictionary {
 
-	@Property(description = "The dictionaries list, the data contained in the first dictionaries will overide the data of the next dictionary, and so on...", required = false)
+	@Property(description = "The list of dictionaries, the data contained in the first dictionaries will override the data of the next dictionary, and so on...", required = false)
 	private List<Dictionary> dictionaries = newArrayList();
 
 	@Override
@@ -36,13 +36,13 @@ public class HierarchicalDictionary extends Dictionary {
 
 	Map<String, String> buildHierarchicalEntries() {
 		final Map<String, String> hentries = Maps.newHashMap();
-		if (dictionaries == null)
-			return hentries;
 
-		for (Dictionary dictionary : reverse(dictionaries)) {
-			final Map<String, String> entries = dictionary.getEntries();
-			if (entries != null) {
-				hentries.putAll(entries);
+		if (dictionaries != null && !dictionaries.isEmpty()) {
+			for (Dictionary dictionary : reverse(dictionaries)) {
+				final Map<String, String> entries = dictionary.getEntries();
+				if (entries != null) {
+					hentries.putAll(entries);
+				}
 			}
 		}
 
--- src/test/java/com/xebialabs/deployit/plugins/dictionary/HierarchicalDictionaryTest.java
+++ src/test/java/com/xebialabs/deployit/plugins/dictionary/HierarchicalDictionaryTest.java
@@ -1,6 +1,5 @@
 package com.xebialabs.deployit.plugins.dictionary;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.xebialabs.deployit.plugin.api.boot.PluginBooter;
 import com.xebialabs.deployit.plugin.api.udm.Dictionary;
@@ -10,6 +9,7 @@ import org.junit.Test;
 
 import java.util.Map;
 
+import static com.google.common.collect.ImmutableList.of;
 import static com.xebialabs.deployit.test.support.TestUtils.newInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -34,7 +34,7 @@ public class HierarchicalDictionaryTest {
 		d1.setProperty("entries", ImmutableMap.of("X", "1", "Y", "1"));
 
 		d2 = newInstance("udm.Dictionary");
-		d2.setProperty("entries", ImmutableMap.of("X", "3", "Y", "3","A","8"));
+		d2.setProperty("entries", ImmutableMap.of("X", "3", "Y", "3", "A", "8"));
 
 		d3 = newInstance("udm.Dictionary");
 		d3.setProperty("entries", ImmutableMap.of("X", "2"));
@@ -48,9 +48,25 @@ public class HierarchicalDictionaryTest {
 	}
 
 	@Test
-	public void testD1D2() {
-		hd.setProperty("dictionaries", ImmutableList.of(d1, d2));
+	public void testEntriesOnly() {
+		hd.setProperty("entries", ImmutableMap.of("foo","bar"));
 		final Map<String, String> entries = hd.getEntries();
+		assertThat(entries.size(),is(1));
+		assertTrue(entries.containsKey("foo"));
+		assertThat(entries.get("foo"), is("bar"));
+	}
+
+	@Test
+	public void testEmpty() {
+		final Map<String, String> entries = hd.getEntries();
+		assertTrue(entries.isEmpty());
+	}
+
+	@Test
+	public void testD1D2() {
+		hd.setProperty("dictionaries", of(d1, d2));
+		final Map<String, String> entries = hd.getEntries();
+		assertThat(entries.size(),is(3));
 		assertTrue(entries.containsKey("X"));
 		assertTrue(entries.containsKey("Y"));
 		assertThat(entries.get("X"), is("1"));
@@ -60,9 +76,10 @@ public class HierarchicalDictionaryTest {
 
 	@Test
 	public void testD1D2Entries() {
-		hd.setProperty("dictionaries", ImmutableList.of(d1, d2));
-		hd.setProperty("entries", ImmutableMap.of("X", "4", "Y", "4","A","9"));
+		hd.setProperty("dictionaries", of(d1, d2));
+		hd.setProperty("entries", ImmutableMap.of("X", "4", "Y", "4", "A", "9"));
 		final Map<String, String> entries = hd.getEntries();
+		assertThat(entries.size(),is(3));
 		assertTrue(entries.containsKey("X"));
 		assertTrue(entries.containsKey("Y"));
 		assertThat(entries.get("X"), is("4"));
@@ -72,8 +89,9 @@ public class HierarchicalDictionaryTest {
 
 	@Test
 	public void testD2D1() {
-		hd.setProperty("dictionaries", ImmutableList.of(d2, d1));
+		hd.setProperty("dictionaries", of(d2, d1));
 		final Map<String, String> entries = hd.getEntries();
+		assertThat(entries.size(),is(3));
 		assertTrue(entries.containsKey("X"));
 		assertTrue(entries.containsKey("Y"));
 		assertThat(entries.get("X"), is("3"));
@@ -82,8 +100,9 @@ public class HierarchicalDictionaryTest {
 
 	@Test
 	public void testD3D1() {
-		hd.setProperty("dictionaries", ImmutableList.of(d3, d1));
+		hd.setProperty("dictionaries", of(d3, d1));
 		final Map<String, String> entries = hd.getEntries();
+		assertThat(entries.size(),is(2));
 		assertTrue(entries.containsKey("X"));
 		assertTrue(entries.containsKey("Y"));
 		assertThat(entries.get("X"), is("2"));
@@ -92,8 +111,9 @@ public class HierarchicalDictionaryTest {
 
 	@Test
 	public void testD4D1() {
-		hd.setProperty("dictionaries", ImmutableList.of(d4, d1));
+		hd.setProperty("dictionaries", of(d4, d1));
 		final Map<String, String> entries = hd.getEntries();
+		assertThat(entries.size(),is(2));
 		assertTrue(entries.containsKey("X"));
 		assertTrue(entries.containsKey("Y"));
 		assertThat(entries.get("X"), is("1"));
@@ -102,8 +122,9 @@ public class HierarchicalDictionaryTest {
 
 	@Test
 	public void testD1D4() {
-		hd.setProperty("dictionaries", ImmutableList.of(d1, d4));
+		hd.setProperty("dictionaries", of(d1, d4));
 		final Map<String, String> entries = hd.getEntries();
+		assertThat(entries.size(),is(2));
 		assertTrue(entries.containsKey("X"));
 		assertTrue(entries.containsKey("Y"));
 		assertThat(entries.get("X"), is("1"));
